79ffd15276531d553824da2189fd9f242161bee1
Aristide Mebodo <mebodoaristide@yahoo.fr>
2021-05-22 02:12:45 +0100

    dbg 1

--- htdocs/core/modules/propale/doc/pdf_azur.modules.php
+++ htdocs/core/modules/propale/doc/pdf_azur.modules.php
@@ -283,7 +283,7 @@ class pdf_azur extends ModelePDFPropales
 		}
 
 		if (count($realpatharray) == 0) $this->posxpicture = $this->posxtva;
-		var_dump($conf->bticket->multidir_output[$conf->entity]);
+		var_dump($conf->propal->multidir_output[$conf->entity]);
 		die;
 		if ($conf->propal->multidir_output[$conf->entity])
 		{
