4fd3dcac1cc697b366be3a6406f815fc0fade74a
Aristide Mebodo <mebodoaristide@yahoo.fr>
2021-05-22 02:38:20 +0100

    dbg 0

--- htdocs/custom/bookticket/doc/pdf_ocean.modules.php
+++ htdocs/custom/bookticket/doc/pdf_ocean.modules.php
@@ -396,6 +396,7 @@ class pdf_ocean extends ModelePDFBticket
 				$pdf->SetTextColor(0, 0, 0);
 
 
+				die('je suis ici');
 				$tab_top = 90 + $top_shift;
 				$tab_top_newpage = (empty($conf->global->MAIN_PDF_DONOTREPEAT_HEAD) ? 42 + $top_shift : 10);
 
@@ -419,7 +420,7 @@ class pdf_ocean extends ModelePDFBticket
 						$tab_top = $nexY + 6;
 					}
 				}
-				die('je suis ici');
+
 				// Affiche notes
 				$notetoshow = empty($object->note_public) ? '' : $object->note_public;
 				if (!empty($conf->global->MAIN_ADD_SALE_REP_SIGNATURE_IN_NOTE))
