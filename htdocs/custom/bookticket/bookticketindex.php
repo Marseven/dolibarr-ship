58e75abfe9facb7eb9f9e087caa6783cb7fe9e98
Aristide Mebodo <mebodoaristide@yahoo.fr>
2021-05-28 07:02:59 +0100

    dbg 7

--- htdocs/custom/bookticket/bookticketindex.php
+++ htdocs/custom/bookticket/bookticketindex.php
@@ -226,17 +226,6 @@ print '<div class="fichecenter fichecenterbis">';
 
 $boxlist .= '<div class="twocolumns">';
 
-$boxlist .= '<div class="firstcolumn fichehalfleft boxhalfleft" id="boxhalfleft">';
-if (!empty($nbworkboardcount))
-{
-	$boxlist .= $boxwork;
-}
-
-$boxlist .= $resultboxes['boxlista'];
-
-$boxlist .= '</div>';
-
-
 if (empty($user->socid) && empty($conf->global->MAIN_DISABLE_GLOBAL_BOXSTATS))
 {
 	// Remove allready present info in new dash board
@@ -281,10 +270,22 @@ if (empty($user->socid) && empty($conf->global->MAIN_DISABLE_GLOBAL_BOXSTATS))
 	}
 }
 
-$boxlist .= '<div class="secondcolumn fichehalfright boxhalfright" id="boxhalfright">';
+$boxlist .= '<div class="firstcolumn fichehalfleft boxhalfleft" id="boxhalfleft">';
 
 $boxlist .= $boxstat;
 $boxlist .= $resultboxes['boxlistb'];
+
+$boxlist .= '</div>';
+
+
+$boxlist .= '<div class="secondcolumn fichehalfright boxhalfright" id="boxhalfright">';
+
+if (!empty($nbworkboardcount))
+{
+	$boxlist .= $boxwork;
+}
+
+$boxlist .= $resultboxes['boxlista'];
 
 $boxlist .= '</div>';
 $boxlist .= "\n";
