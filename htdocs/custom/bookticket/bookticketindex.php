87a9bb5f0304596585240d476846663e946f471b
Aristide Mebodo <mebodoaristide@yahoo.fr>
2021-05-28 06:53:43 +0100

    dbg 2

--- htdocs/custom/bookticket/bookticketindex.php
+++ htdocs/custom/bookticket/bookticketindex.php
@@ -217,7 +217,7 @@ if ($user->rights->bookticket->bticket->read)
 
 print '<div class="clearboth"></div>';
 
-print '<div class="fichethirdleft fichecenterbis">';
+print '<div class="ficheleft fichecenterbis">';
 
 
 /*
