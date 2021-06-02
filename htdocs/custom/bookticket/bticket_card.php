6b4b1ab3f3398b380e0b2cce6962462b1a51d691
Aristide Mebodo <mebodoaristide@yahoo.fr>
2021-06-02 23:58:24 +0100

    dbg 8

--- htdocs/custom/bookticket/bticket_card.php
+++ htdocs/custom/bookticket/bticket_card.php
@@ -922,10 +922,8 @@ if (is_object($objcanvas) && $objcanvas->displayCanvasExists($action)) {
 
 			$passenger .= '</select>';
 
-			$passenger .= '<script>';
-			$passenger .= "$(document).ready(function() {
-							$('.passenger').select2();
-						});";
+			$passenger .= '<script type="text/javascript">';
+			$passenger .= "$(document).ready(function() {$('.passenger').select2();});";
 			$passenger .= '</script>';
 
 			print $passenger;
