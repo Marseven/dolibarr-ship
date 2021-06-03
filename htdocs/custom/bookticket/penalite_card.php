b4049c966f5e19eb014317254cacb9af909d7208
Aristide Mebodo <mebodoaristide@yahoo.fr>
2021-06-03 21:52:21 +0100

    dbg 1

--- htdocs/custom/bookticket/penalite_card.php
+++ htdocs/custom/bookticket/penalite_card.php
@@ -581,7 +581,7 @@ if (is_object($objcanvas) && $objcanvas->displayCanvasExists($action)) {
 			$number = "0123456789";
 			$code = substr(str_shuffle(str_repeat($number, 6)), 0, 6);
 			$tmpref = "DVM-PN-".$code;
-			print '<tr><td class="titlefieldcreate fieldrequired">'.$langs->trans("Ref").'</td><td colspan="3"><input name="ref" class="maxwidth200" maxlength="128" value="'.dol_escape_htmltag($tmpref).'" disabled></td></tr>';
+			print '<tr><td class="titlefieldcreate fieldrequired">'.$langs->trans("Ref").'</td><td colspan="3"><input name="ref" class="maxwidth200" maxlength="128" value="'.$tmpref.'"></td></tr>';
 
 			// bticket
 			print '<tr><td class="titlefieldcreate">'.$langs->trans("Bticket").'</td>';
