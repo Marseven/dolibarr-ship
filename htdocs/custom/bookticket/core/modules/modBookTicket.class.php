a908bb9fb4323d248c90f5a4ca79e94ed37796a8
Aristide Mebodo <mebodoaristide@yahoo.fr>
2021-06-22 20:41:18 +0100

    dbg 4

--- htdocs/custom/bookticket/core/modules/modBookTicket.class.php
+++ htdocs/custom/bookticket/core/modules/modBookTicket.class.php
@@ -565,22 +565,6 @@ class modBookTicket extends DolibarrModules
 			'user'=>2,				                // 0=Menu for internal users, 1=external users, 2=both
 		);
 
-		$this->menu[$r++]=array(
-			'fk_menu'=>'fk_mainmenu=bookticket,fk_leftmenu=ticket',	    // '' if this is a top menu. For left menu, use 'fk_mainmenu=xxx' or 'fk_mainmenu=xxx,fk_leftmenu=yyy' where xxx is mainmenucode and yyy is a leftmenucode
-			'type'=>'left',			                // This is a Left menu entry
-			'titre'=>'AddBTicket',
-			'mainmenu'=>'bookticket',
-			'leftmenu'=>'bookticket_ticket_new',
-			'url'=>'/bookticket/bticket_card.php?action=create',
-			'langs'=>'bookticket@bookticket',	        // Lang file to use (without .lang) by module. File must be in langs/code_CODE/ directory.
-			'position'=>1000+$r,
-			'enabled'=>'$conf->bookticket->enabled',  // Define condition to show or hide menu entry. Use '$conf->bookticket->enabled' if entry must be visible if module is enabled. Use '$leftmenu==\'system\'' to show if leftmenu system is selected.
-			//'perms'=>'1', // Use 'perms'=>'$user->rights->bookticket->myobject->read' if you want your menu with a permission rules
-			'perms'=>'$user->rights->bookticket->bticket->write',			                // Use 'perms'=>'$user->rights->bookticket->level1->level2' if you want your menu with a permission rules
-			'target'=>'',
-			'user'=>2,				                // 0=Menu for internal users, 1=external users, 2=both
-		);
-
 		//Penalite
 		$this->menu[$r++]=array(
 			'fk_menu'=>'fk_mainmenu=bookticket,fk_leftmenu=ticket',	    // '' if this is a top menu. For left menu, use 'fk_mainmenu=xxx' or 'fk_mainmenu=xxx,fk_leftmenu=yyy' where xxx is mainmenucode and yyy is a leftmenucode
