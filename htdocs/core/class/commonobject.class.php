46184886835cd315d976af999cc9081f3d8c7cb8
Aristide Mebodo <mebodoaristide@yahoo.fr>
2021-06-29 12:03:59 +0100

    dbg 1

--- htdocs/core/class/commonobject.class.php
+++ htdocs/core/class/commonobject.class.php
@@ -7752,11 +7752,10 @@ abstract class CommonObject
 			$sql = 'INSERT INTO '.MAIN_DB_PREFIX.$this->table_element;
 			$sql .= ' ('.implode(", ", $keys).')';
 			$sql .= ' VALUES ('.implode(", ", $values).')';
-
-			var_dump($sql);die;
 			
 			$res = $this->db->query($sql);
 			var_dump($this->db->lasterror());
+			var_dump($res);die;
 			if ($res === false) {
 				$error++;
 				$this->errors[] = $this->db->lasterror();
