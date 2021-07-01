382ffd895ae75cc526036e5ff2320346924913df
Aristide Mebodo <mebodoaristide@yahoo.fr>
2021-07-01 01:45:25 +0100

    dbg 8

--- htdocs/custom/bookticket/bticket_card.php
+++ htdocs/custom/bookticket/bticket_card.php
@@ -908,7 +908,7 @@ if (is_object($objcanvas) && $objcanvas->displayCanvasExists($action)) {
 			print '<table class="border allwidth">';
 
 			// Ref
-			print '<tr><td class="titlefieldcreate fieldrequired">'.$langs->trans("Ref").'</td><td colspan="3"><input name="ref" class="maxwidth200" maxlength="128" value="'.dol_escape_htmltag($object->ref).'" disabled></td></tr>';
+			print '<tr><td class="titlefieldcreate fieldrequired">'.$langs->trans("Ref").'</td><td colspan="3"><input name="ref" class="maxwidth200" maxlength="128" value="'.dol_escape_htmltag($object->ref).'"></td></tr>';
 
 			//travel
 			print '<tr><input type="hidden" name="fk_travel" value="'.$object->fk_travel.'"></tr>';
